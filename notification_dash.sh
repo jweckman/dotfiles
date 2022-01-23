7ff906105feb4d10fb836cbce5837106c9e0c473
Joakim Weckman <joakim.weckman@gmail.com>
2022-01-23 19:24:26 +0200

    added battery level to main notification

--- notification_dash.sh
+++ notification_dash.sh
@@ -3,8 +3,15 @@
 dow=$(date '+%A')
 date=$(date '+%Y-%m-%d');
 time=$(date '+%H:%M:%S');
-msg="$time
+power_level="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage:)"
+pl_arr=($power_level)
+pl_percentage=${pl_arr[1]}
+
+msg="${time}
 
 $dow
-$date"
+$date
+Battery: $pl_percentage
+"
+
 notify-send "$msg"
