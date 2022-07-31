38da3a77257a1d3cf606da1784854096de882b2b
Joakim Weckman <joakim.weckman@gmail.com>
2022-07-31 11:05:49 +0300

    jwqtnotify center and prettify

--- jwqtnotify.py
+++ jwqtnotify.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtGui import QFont
 
 class MainWindow(QWidget):
 
@@ -12,10 +14,11 @@ class MainWindow(QWidget):
 
         self.layout = QVBoxLayout()
         self.label = QLabel(self.stdin_to_string())
-
+        self.label.setFont(QFont("Arial", 16))
         self.layout.addWidget(self.label)
         self.setWindowTitle("JW personal notifications")
         self.setLayout(self.layout)
+        self.center()
 
     def stdin_to_string(self):
         res = ''
@@ -24,9 +27,28 @@ class MainWindow(QWidget):
 
         return res
 
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    style = """
+        QWidget{
+            background: #262D37;
+        }
+        QLabel{
+            color: #fff;
+        }
+        QLineEdit {
+                padding 1px;
+                color #fff;
+                border: 2px solid #fff;
+                border-radius: 8px;
+    """
+    app.setStyleSheet(style)
     app.setApplicationName("jwqtnotify")
     mw = MainWindow()
     mw.show()
