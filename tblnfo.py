5c788745adebcd461f3646f669fc64fe852aa990
Joakim Weckman <joakim.weckman@gmail.com>
2022-12-10 20:47:53 +0200

    tblnfo add stdin support

--- tblnfo.py
+++ tblnfo.py
@@ -1,50 +1,85 @@
 #!/usr/bin/env python
 import pandas as pd
-from sys import argv
-import re
+from sys import argv, stdin
+import select
 from pathlib import Path
-from typing import List
+from typing import List, TextIO
 import warnings
 
+def read_data() -> pd.DataFrame:
+    '''Function that dynamically picks data from
+    stdin or argv depending on what is available. Stdin
+    only supports csv content at the moment'''
+    if select.select([stdin, ], [], [], 0.0)[0]:
+        file_object = stdin
+    else:
+        file_name = argv[1]
+        file_object = Path(file_name)
+    df = read_table_file(file_object)
+
+    return df
+
 # File detection functions
-def read_table_file(f: Path) -> pd.DataFrame | None:
+def read_table_file(f: Path | TextIO) -> pd.DataFrame:
     ''' Try various ways of autodetecting
     file type and other pieces of information
     required and parse the file into a
     pandas dataframe'''
     df = None
-    match f.suffix:
-        case ".csv":
-            print("Filetype is: csv\n")
-            sep = detect_csv_sep(f)
-            df = pd.read_csv(
-                f,
-                sep = sep,
-            )
-        case ".xls" | ".xlsx":
-            print("Filetype is: MS excel\n")
-            df = pd.read_excel(f)
+    if isinstance(f, Path):
+        match f.suffix:
+            case ".csv":
+                df = read_csv_file(f)
+            case ".xls" | ".xlsx":
+                print("Filetype is: MS excel\n")
+                df = pd.read_excel(f)
+    else:
+        df = read_csv_file(f)
+    if not df:
+        raise ValueError(
+            "Passed file could not be matched as a csv or MS excel file"
+        )
 
     return df
 
-def detect_csv_sep(f: Path) -> str:
+def detect_csv_sep(f: Path | TextIO) -> str:
     '''Initial support for autodetecting:
     - comma
     - semicolon
     Defaults to ","
     '''
     sep = ','
-    with open(f, 'r') as fp:
-        l1 = fp.readline()
-        l2 = fp.readline()
-        lines = [l1, l2]
-        counts = {',': 0, ';': 0}
-        for l in lines:
-            for k in counts:
-                counts[k] = l.count(k)
-        top_seps = [key for key, value in counts.items() if value == max(counts.values())]
-        sep = top_seps[0]
+    if isinstance(f, Path):
+        fp = open(f, 'r')
+    else:
+        fp = f
+    l1 = fp.readline()
+    l2 = fp.readline()
+    lines = [l1, l2]
+    csv_sep_detect_logic(lines)
+    if isinstance(f, Path):
+        fp.close()
+
     return sep
+
+def csv_sep_detect_logic(lines: List) -> str:
+    counts = {',': 0, ';': 0}
+    for l in lines:
+        for k in counts:
+            counts[k] = l.count(k)
+    top_seps = [key for key, value in counts.items() if value == max(counts.values())]
+    sep = top_seps[0]
+
+    return sep
+
+def read_csv_file(f: Path | TextIO) -> pd.DataFrame:
+    print("Filetype is: csv\n")
+    sep = detect_csv_sep(f)
+    df = pd.read_csv(
+        f,
+        sep = sep,
+    )
+    return df
 
 
 # Column type assignment & cleanup functions
@@ -122,9 +157,7 @@ def column_level_overview_text(
 
 
 if __name__ == '__main__':
-    file_name = argv[1]
-    file_path = Path(file_name)
-    dfm = read_table_file(file_path)
+    dfm = read_data()
     assign_df_types(dfm)
     candidates = get_analysis_candidates(dfm)
     column_level_overview_text(dfm, candidates)
