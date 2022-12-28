b67b87cf46a3ed241ecc5be65ae8b09eeb68907c
Joakim Weckman <joakim.weckman@gmail.com>
2022-12-28 20:11:03 +0200

    nvim - fix duplicate find_files

--- nvim/init.lua
+++ nvim/init.lua
@@ -326,12 +326,11 @@ pcall(require('telescope').load_extension, 'fzf')
 -- Telescope keymaps
 vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles', layout_strategy = wide_layout })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles'})
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 
 vim.keymap.set('n', '<leader>fg', "<cmd>lua require'telescope'.extensions.live_grep_args.live_grep_args()<CR>", opts)
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
 vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume previous query' })
 vim.keymap.set('n', '<leader>gb', require('telescope.builtin').git_bcommits, { desc = '' })
