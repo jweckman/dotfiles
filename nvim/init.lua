fd10ff76f224c848756d88e1b1bb300cfeb72946
Joakim Weckman <joakim.weckman@gmail.com>
2023-04-22 14:52:38 +0300

    nvim - add back dap functions
    Issue: writing causes lsp errors that should be figured out

--- nvim/init.lua
+++ nvim/init.lua
@@ -630,12 +630,105 @@ vim.api.nvim_create_user_command("RegDiff", function(ctx)
   vim.cmd("wincmd p")
 end, { bar = true, nargs = "*" })
 
+-- DAP custom functions
+
+local get_docker_service_ips = function()
+    local handle = io.popen("docker ps -q | xargs -n 1 docker inspect --format '{{ .Name }} {{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}}' | sed 's#^/##';")
+    local service_ip_str = handle:read("*a")
+    handle:close()
+    local res = {}
+    for line in service_ip_str:gmatch("[^\r\n]+") do
+        local service_ip = {}
+        for w in line:gmatch("%S+") do
+            service_ip[#service_ip + 1] = w
+        end
+        res[service_ip[1]] = service_ip[2]
+    end
+    return res
+end
+
+DAPATTACH = {}
+
+DAPATTACH.attach_python_debugger = function()
+    local docker_service_ips = get_docker_service_ips()
+    local adapter_config = {
+        ['/home/joakim/code/docker-debug-python'] = {
+            docker_service_name_substr = 'debug-python', 
+            adapter = {
+                host = nil,
+                port = '12345'
+            },
+            remote_root = '/home/joakim/data-pydebugdemo'
+        },
+        ['/home/joakim/code/demo16/odoo/src'] = {
+            docker_service_name_substr = 'odoo16', 
+            adapter = {
+                host = nil,
+                port = '12345'
+            },
+            remote_root = '/odoo/src'
+        }
+    }
+    local config = nil
+    for candidate_parent, conf in pairs(adapter_config) do
+        patternized_cp = string.gsub(candidate_parent, '-', '.')
+        local is_substr = string.match(vim.fn.getcwd(), patternized_cp)
+        if is_substr ~= nil then
+            config = conf
+            break
+        end
+    end
+    if config == nil then
+        error("No configured local path is a substring of current neovim path")
+    end
+    local dap = require "dap"
+    docker_service_name_substr = config['docker_service_name_substr']
+    local adapter = config['adapter']
+    if docker_service_name_substr ~= nil then
+        for candidate, ip in pairs(docker_service_ips) do
+            patternized_name = string.gsub(docker_service_name_substr, '-', '.')
+            local is_substr = string.match(candidate, patternized_name)
+            if is_substr ~= nil then
+                adapter['host'] = ip
+                break
+            end
+        end
+    end
+    pythonAttachAdapter = {
+        type = "server";
+        host = adapter;
+        port = tonumber(adapter['port']);
+    }
+    pythonAttachConfig = {
+        type = "python";
+        request = "attach";
+        connect = {
+            port = tonumber(adapter['port']);
+            host = host;
+        };
+        mode = "remote";
+        name = "Remote Attached Debugger";
+        cwd = vim.fn.getcwd();
+        pathMappings = {
+            {
+                localRoot = vim.fn.getcwd(); -- Wherever your Python code lives locally.
+                remoteRoot = config['remote_root']; -- Wherever your Python code lives in the container.
+            };
+        };
+    }
+    local session = dap.attach(adapter, pythonAttachConfig)
+    if session == nil then
+        io.write("Error launching adapter");
+    end
+    dap.repl.open()
+end
+
 -- Plugin keybindings
 
 -- DAP keybindings
 vim.keymap.set('n', '<leader>da', "<cmd>lua DAPATTACH.attach_python_debugger()<CR>", opts)
 vim.keymap.set('n', '<leader>dc', "<cmd>lua require'dap'.continue()<CR>", opts)
-vim.keymap.set('n', '<leader>dov', "<cmd>lua require'dap'.step_over()<CR>", opts)
+vim.keymap.set('n', '<leader>do', "<cmd>lua require'dap'.step_over()<CR>", opts)
 vim.keymap.set('n', '<leader>dsi', "<cmd>lua require'dap'.step_into()<CR>", opts)
 vim.keymap.set('n', '<leader>dso', "<cmd>lua require'dap'.step_out()<CR>", opts)
 vim.keymap.set('n', '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
