3d62be1eed8e9cb237ae507b6bdd044e8faeb4d8
Joakim Weckman <joakim.weckman@gmail.com>
2023-09-26 21:49:29 +0300

    tblnfo - migrate to polars from pandas

--- tblnfo.py
+++ tblnfo.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
-import pandas as pd
 import polars as pl
 import sys
-import select
 import io
 from pathlib import Path
-from typing import List, TextIO
-import warnings
 import argparse
-from enum import Enum
+
 parser = argparse.ArgumentParser()
 parser.add_argument('infile', nargs='?')
 args = parser.parse_args()
-
-class ContentType(Enum):
-    FILE_NAME = 1
-    CSV = 2
 
 def read_data() -> pl.DataFrame:
     '''Function that dynamically picks data from
@@ -32,18 +24,22 @@ def read_data() -> pl.DataFrame:
 
 # File detection functions
 def read_table_file(f: io.StringIO | Path) -> pl.DataFrame:
-    ''' Try various ways of autodetecting
+    '''
+    Try various ways of autodetecting
     file type and other pieces of information
     required and parse the file into a
-    pandas dataframe'''
+    dataframe.
+    '''
     df = None
     if isinstance(f, Path):
         match f.suffix:
+            case ".xls":
+                raise ValueError("Old .xls files are not supported. Please convert to .xlsx or .csv")
             case ".csv":
                 df = read_csv_file(f)
-            case ".xls" | ".xlsx":
+            case ".xlsx":
                 print("Filetype is: MS excel\n")
-                df = pd.read_excel(f)
+                df = pl.read_excel(f)
     else:
         df = read_csv_file(f)
     if not isinstance(df, pl.DataFrame):
@@ -71,7 +67,7 @@ def detect_csv_sep(f: Path | io.StringIO ) -> str:
 
     return sep
 
-def csv_sep_detect_logic(lines: List) -> str:
+def csv_sep_detect_logic(lines: list) -> str:
     counts = {',': 0, ';': 0}
     for l in lines:
         for k in counts:
@@ -85,7 +81,12 @@ def read_csv_file(f: Path | io.StringIO) -> pl.DataFrame:
     print("Filetype is: csv\n")
     sep = detect_csv_sep(f)
 
-    df = pl.read_csv(f, infer_schema_length=10000, separator=sep)
+    df = pl.read_csv(
+        f,
+        infer_schema_length = 10000,
+        separator = sep,
+        try_parse_dates = True
+    )
     if isinstance(f, io.StringIO):
         f.close()
     return df
@@ -94,22 +95,34 @@ def read_csv_file(f: Path | io.StringIO) -> pl.DataFrame:
 # Column type assignment & cleanup functions
 def assign_df_types(df: pl.DataFrame) -> None:
     '''Changes types in place'''
-    print(df.schema)
     for column, tpe in df.schema.items():
         base_type = tpe.base_type()
-        print(tpe.base_type())
         if base_type == pl.Utf8:
-            uq = pl.col(column).drop_nans().drop_nulls().unique()
+            uq = df.get_column(column).drop_nans().drop_nulls().unique()
             # Try to cast to date
-            if any([x in str(uq[0]) for x in ['.', '-', '/']]):
-                try:
-                    # TODO: cast to date
-                    df.col(column)
-                except:
-                    pass
+            first_str = str(uq.item(0))
+            if any([x in first_str for x in ['.', '-']]):
+                assign_col_datetime(df, column, first_str)
             if base_type != pl.Datetime:
                 # TODO: think of what other casts may be useful
                 pass
+
+def assign_col_datetime(
+        df: pl.DataFrame,
+        column_name: str,
+        sample_value: str,
+    ):
+    '''Applied in-place if successful. Fails silently and keeps string format'''
+    date_format = None
+    if '-' in sample_value:
+        date_format = '%Y-%m-%d'
+    elif '.' in sample_value:
+        date_format = '%Y.%m.%d'
+    try:
+        date_series = df.get_column(column_name).str.to_datetime(date_format)
+        df = df.with_columns(date_series.alias(column_name))
+    except:
+        pass
 
 def string_column_cleanup(df: pl.DataFrame) -> None:
     # TODO: Add generic cleanup here
@@ -118,69 +131,74 @@ def string_column_cleanup(df: pl.DataFrame) -> None:
 
 # Column analysis candidate finding functions
 def get_analysis_candidates(df: pl.DataFrame) -> dict[str, list]:
-    col_candidates = dict(keys=df.columns)
-    for col in df.columns:
-        col_candidates[col] = get_col_analysis_candidates(df[col])
+    columns = list(df.schema.keys())
+    col_candidates = dict(keys=columns)
+    for column in columns:
+        col_candidates[column] = get_col_analysis_candidates(df.get_column(column))
     return col_candidates
 
-def get_col_analysis_candidates(s: pd.Series) -> list:
+def get_col_analysis_candidates(s: pl.Series) -> list:
     candidates = []
-    if 'datetime64' in str(s.dtype):
+    if s.dtype in [pl.Datetime, pl.Date]:
         candidates.append('numeric_cron_date')
-    if s.dtype in [int, float]:
+    if s.dtype in [pl.Int64, pl.Float64]:
         if _detect_year_col(s):
             candidates.append('year')
         else:
             candidates.append('numeric')
             candidates.append('basic_stats')
-    if s.dtype == 'object':
+    if s.dtype == pl.Utf8:
         candidates.append('grouper')
         candidates.append('value_counts')
 
     return candidates
 
-def _detect_year_col(series: pd.Series) -> bool:
-    if all(series.dropna().between(1900, 2100)):
+def _detect_year_col(series: pl.Series) -> bool:
+    if all(series.drop_nulls().drop_nans().is_between(1900, 2100)):
         return True
     return False
 
 # Analysis functions
 
-def _col_nan_percentage(s: pd.Series) -> float | None:
+def _col_empty_percentage(s: pl.Series) -> float | None:
     if len(s) > 0:
-        return s.isna().tolist().count(True) / len(s)
+        original_length = len(s)
+        return (original_length - len(s.drop_nulls().drop_nans())) / original_length * 100
+    return None
 
 def column_level_overview_text(
             df: pl.DataFrame,
             analysis_candidates: dict[str, list]
         ) -> None:
-    for i, col in enumerate(df.columns):
-        print(f"SUMMARY FOR COLUMN: {col}, index: {i} [{df[col].dtype}]")
-        if all(df[col].isna()):
+    divider = "----------------------------------------------"
+    for i, (column_name, tpe) in enumerate(df.schema.items()):
+        column: pl.Series = df.get_column(column_name)
+        print(f"SUMMARY FOR COLUMN: {column_name}, index: {i} [{tpe.base_type()}]")
+        if len(column.drop_nulls().drop_nans()) == 0:
             print("Column is completely empty")
-            print("--------------------------------------------")
+            print(divider)
             break
-        nan_percentage = _col_nan_percentage(df[col]) * 100
-        if nan_percentage:
-            print(f"NaN percentage: {nan_percentage:.4f} %")
+        empty_percentage = _col_empty_percentage(column)
+        if empty_percentage:
+            print(f"Empty percentage: {empty_percentage:.4f} %")
         else:
-            print(f"No NaN values")
-        candidates = analysis_candidates[col]
+            print(f"No empty values")
+        candidates = analysis_candidates[column_name]
         if 'year' in candidates:
-            print(f"Contains years between: {df[col].min()} - {df[col].max()}")
+            print(f"Contains years between: {column.min()} - {column.max()}")
         if 'numeric_cron_date' in candidates:
-            print(f"Date range: {df[col].dt.date.min()} - {df[col].dt.date.max()}")
+            print(f"Date range: {column.dt.date().min()} - {column.dt.date().max()}")
         elif 'basic_stats' in candidates and 'numeric' in candidates:
-            median = df[col].median()
+            median = column.median()
             print(f"median     {median:.6f}")
-            print(df[col].describe())
+            print(column.describe())
         elif 'grouper' in candidates and 'value_counts' in candidates:
-            print(df[col].value_counts())
-        print("--------------------------------------------")
+            print(column.value_counts().sort(by='counts', descending=True))
+        print(divider)
 
 
 if __name__ == '__main__':
     dfm = read_data()
     assign_df_types(dfm)
-    # candidates = get_analysis_candidates(dfm)
-    # column_level_overview_text(dfm, candidates)
+    candidates = get_analysis_candidates(dfm)
+    column_level_overview_text(dfm, candidates)
