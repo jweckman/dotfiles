1f95699af25e0eee65cbbe11424229a5e1a51d62
Joakim Weckman <joakim.weckman@gmail.com>
2025-01-02 18:43:20 +0200

    jwqtnotify - upgrade to pyqt6

--- scripts/jwqtnotify.py
+++ scripts/jwqtnotify.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
+
 import sys
-from pathlib import Path
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QDesktopWidget
-from PyQt5.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
+
 
 class MainWindow(QWidget):
 
@@ -16,23 +14,27 @@ class MainWindow(QWidget):
         self.layout = QVBoxLayout()
         self.label = QLabel(self.stdin_to_string())
         self.label.setFont(QFont("Arial", 16))
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.label)
         self.setWindowTitle("jw_personal_notifications")
         self.setLayout(self.layout)
-        self.center()
+        self.adjust_size_and_center()
 
     def stdin_to_string(self):
         res = ''
-        for l in sys.stdin:
-            res = res + l
+        for line in sys.stdin:
+            res += line
 
         return res
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def adjust_size_and_center(self):
+        self.adjustSize()
+        screen = QApplication.primaryScreen().geometry()
+        self.move(
+            (screen.width() - self.width()) // 2,
+            (screen.height() - self.height()) // 2
+        )
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -44,14 +46,14 @@ if __name__ == "__main__":
             color: #EEEEEE;
         }
         QLineEdit {
-                padding 1px;
-                color #fff;
-                border: 2px solid #fff;
-                border-radius: 8px;
+            padding: 1px;
+            color: #fff;
+            border: 2px solid #fff;
+            border-radius: 8px;
+        }
     """
     app.setStyleSheet(style)
     app.setApplicationName("jwqtnotify")
     mw = MainWindow()
     mw.show()
-    sys.exit(app.exec_())
-
+    sys.exit(app.exec())
