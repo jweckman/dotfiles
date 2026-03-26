866437d49eff031757c29bf9f38c4f883b0cffd5
Joakim Weckman <joakim.weckman@gmail.com>
2026-03-26 19:24:20 +0200

    nvim - add treesitter context config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -863,6 +863,20 @@ require("lazy").setup({
 		end,
 	},
 
+	-- USERCONFIG: Treesitter context
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+			max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+			line_numbers = true,
+			multiline_threshold = 20, -- Maximum number of lines to show for a single context
+			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+		},
+	},
+
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
